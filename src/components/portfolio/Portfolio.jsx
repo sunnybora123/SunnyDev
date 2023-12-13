b4a719fd037856ed5fdfc6825b62d916d0ec2905
sunnybora123 <sunnybora986@gmail.com>
2023-12-13 17:55:18 +0530

    second commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,6 @@
 import { useRef } from "react";
 import "./portfolio.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
-import {Link} from "react-router-dom";
 
 const items = [
   {
